6d65f45dd8d71376565ba7187e61251f6a89e41a
arikan-u <reecanbusinessonly@gmail.com>
2020-03-25 10:10:03 +0100

    displays username, courses and even numbers

---
+++ js-assignment.js
@@ -0,0 +1,15 @@
+let name = "Arikan Udoka";
+console.log("My name is: "+ name);
+
+let fe_courses = ["HTML", "CSS", "Javascript"];
+let des_courses = ["UI/UX"]
+console.log("I am taking these courses in front end: " + fe_courses + " and a " + des_courses + " course from design.")
+
+let num = 200
+ 
+for (let i=1; i<=200; i++) {
+    if( i % 2 === 0 ){
+        console.log(i);
+        
+        }
+    }
